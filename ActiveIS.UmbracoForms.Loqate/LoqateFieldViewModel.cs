6cd67f11111cf84b2a9f1c8bc9123e4eebaa7240
Aaron <aaron.sadler@activeis.net>
2020-07-09 11:13:02 +0100

    Created View Model for Loqate with FormId

---
+++ ActiveIS.UmbracoForms.Loqate/LoqateFieldViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Umbraco.Forms.Mvc.Models;
+
+namespace ActiveIS.UmbracoForms.Loqate
+{
+    public class LoqateFieldViewModel : FieldViewModel
+    {
+        public Guid FormId { get; set; }
+    }
+}
